f2de1c2492bef5bd47f6c4ddc3b68252a2555edb
unknown <mariolyv7@gmail.com>
2022-10-03 13:22:08 -0400

    first commit

---
+++ assignments.js
@@ -0,0 +1,34 @@
+//JAVASCRIPT FUNDAMENTALS PART 1
+
+//VALUES AND VARIABLES
+//Declarando multiples variables y sus valores
+let country = 'Bolivia', city = 'La Paz';
+//Declarando variables y sus valores individualmente
+let continent = 'South America';
+let population = 110000;
+//Mostrando valores en consola
+console.log(country, city, continent, population);
+
+//DATA TYPES
+let isIsland = false;
+//let language;
+//Mostrando tipo de datos en consola
+console.log(typeof isIsland, typeof population, typeof country);
+
+//LET, CONST, VAR
+let language = "spanish";
+const firstName = 'Marioly';
+//firstName = 'Steffany';
+console.log(firstName);
+//No se puede cambiar el valor de un valor const
+
+//BASIC OPERATORS
+console.log(population / 2);
+console.log(++population);
+console.log(population > 6000000);
+console.log(population < 33000000);
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(description);
+
+//STRINGS AND TEMPLATE LITERALS
---
+++ script.js
@@ -0,0 +1,31 @@
+/*let molly="molly"
+console.log(molly)
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+
+const age = 30;
+age = 31;
+*/
+
+const firstName = 'Marioly';
+const job = 'teacher';
+const birthYear = 1999;
+const year = 2022;
+
+const marioly = "I'm "+firstName+ ', a '+ (year-birthYear) + " year old "+job;
+console.log(marioly);
+
+const mariolyNew = `I'm ${firstName}, a ${year-birthYear} year old ${job}`;
+console.log(mariolyNew);
+
+console.log('String with \n\
+multiple\n\
+lines');
+
+console.log(`String 
+multiple 
+lines`);
